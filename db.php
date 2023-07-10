0a5e257e16ecbec866b019073f4a0008e0859952
테스트 <test@gmail.com>
2023-07-10 17:06:51 +0900

    0710.12

--- db.php
+++ db.php
@@ -1,14 +1,15 @@
 <?php
 
-	header('Content-Type: text/html; charset=utf-8'); // utf-8인코딩
+header('Content-Type: text/html; charset=utf-8'); // utf-8인코딩
 
-	// localhost = DB주소, web=DB계정아이디, 1234=DB계정비밀번호, post_board="DB이름"
-	$db = new mysqli("localhost","alex","!qas123456789","jin"); 
-	$db->set_charset("utf8");
+// DB 주소, DB 계정 아이디, DB 계정 비밀번호, DB 이름
+$db = new mysqli("20.41.116.36", "alex", "!qas123456789", "jin");
+$db->set_charset("utf8");
 
-	function mq($sql)
-	{
-		global $db;
-		return $db->query($sql);
-	}
+function mq($sql)
+{
+    global $db;
+    return $db->query($sql);
+}
+
 ?>
